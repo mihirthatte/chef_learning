72ef579915ab427bff9f6e3a1f2ab3024d0eb962
Mihir Thatte <mthatte@salesforce.com>
2018-07-05 17:17:50 -0400

    Correct python script for bulk insert into elasticsearch

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,7 +8,7 @@
 directory '/var/tmp/generator' do
 	owner 'root'
 	group 'root'
-	mode '0776'
+	mode '755'
 	action :create
 end
 
@@ -57,4 +57,7 @@ end
 #execute 'create es index' do
 #	command 'curl -X PUT "localhost:9200/test_es" -H "Content-Type: application/json" -d\' { "settings": { "number_of_shards": 1, "number_of_replicas": 0 }, "mappings": { "examplecase": { "properties": { "first_name": { "type": "text" }, "date_of_birth": { "type": "text" }, "last_name": { "type": "text" }, "location": { "type": "text" }, "email": { "type": "text" } } } } }\''
 #end
+
+
+#curl -X GET "localhost:9200/test_es/_search?pretty=true"
 
