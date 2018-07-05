aef8828b1daf1d19c939884a0bae15bdf1b65da8
Mihir Thatte <mthatte@salesforce.com>
2018-07-05 16:35:45 -0400

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# random_users
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: random_users
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
