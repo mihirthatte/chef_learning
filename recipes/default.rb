2d67b7442c62a03fc8e636888fb3e42aefc45fce
Mihir Thatte <mthatte@salesforce.com>
2018-07-06 11:40:12 -0400

    Added inspec tests to verify

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,32 +5,35 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
 
+#Create a directory - 
 directory '/var/tmp/generator' do
-	owner 'root'
-	group 'root'
+	owner 'vagrant'
+	group 'vagrant'
 	mode '755'
 	action :create
 end
 
+#Installing httpd, python-pip, java sdk, and elasticsearch = 
 package 'httpd'
 
 package 'epel-release'
 
 package 'python-pip'
 
+package 'java-1.8.0-openjdk-devel'
 
+template '/etc/yum.repos.d/elasticsearch.repo' do
+        source 'elasticsearch.repo.erb'
+end
+
+package 'elasticsearch'
+
+
+#Starting httpd and elasticsearch services
 service 'httpd' do
 	action [:enable, :start]
 end
 
-template '/etc/yum.repos.d/elasticsearch.repo' do
-	source 'elasticsearch.repo.erb'
-end
-
-package 'java-1.8.0-openjdk-devel'
-
-package 'elasticsearch'
-	
 service 'elasticsearch' do
 	action :enable
 	action :start
@@ -39,25 +42,35 @@ end
 
 template '/var/www/html/index.html' do
         source 'index.html.erb'
+	mode '0755'
+	owner 'vagrant'
+	group 'vagrant'
 end
 
 template '/var/tmp/generator/populate_data.py' do
         source 'populate_data.py.erb'
+	mode '0755'
+	owner 'vagrant'
+	group 'vagrant'
+end
+
+
+#Installing python modules - elasticsearch and request
+execute 'install pip packages' do
+	command 'pip install elasticsearch'
 end
 
 execute 'install pip packages' do
-  command 'pip install elasticsearch'
+	command 'pip install requests'
 end
 
-execute 'install pip packages' do
-  command 'pip install requests'
+cron 'python script' do
+	hour '5'
+	minute '0'
+	command 'python /var/tmp/generator/populate_data.py'
 end
 
-
-#execute 'create es index' do
-#	command 'curl -X PUT "localhost:9200/test_es" -H "Content-Type: application/json" -d\' { "settings": { "number_of_shards": 1, "number_of_replicas": 0 }, "mappings": { "examplecase": { "properties": { "first_name": { "type": "text" }, "date_of_birth": { "type": "text" }, "last_name": { "type": "text" }, "location": { "type": "text" }, "email": { "type": "text" } } } } }\''
+#Running the pythpn script - 
+#execute 'Run python script' do
+#	command 'python /var/tmp/generator/populate_data.py'
 #end
-
-
-#curl -X GET "localhost:9200/test_es/_search?pretty=true"
-
--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -5,14 +5,66 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+
+#httpd should be installed and running
+describe package('httpd') do
+  it { should be_installed }
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe service('httpd') do
+  it { should be_running }
 end
+
+
+#Elasticsearch should be installed and running
+describe package('elasticsearch') do
+  it { should be_installed }
+end
+
+describe service('elasticsearch') do
+  it { should be_running }
+end
+
+#jdk 1.8. python-pip, and epel should be installed 
+describe package('java-1.8.0-openjdk-devel') do
+  it { should be_installed }
+end
+
+describe package('python-pip') do
+  it { should be_installed }
+end
+
+describe package('epel-release') do
+  it { should be_installed }
+end
+
+#Test to make sure python moduiles are installed - 
+describe pip('elasticsearch') do
+  it { should be_installed }
+end
+
+describe pip('requests') do
+  it { should be_installed }
+end
+
+describe file('/var/www/html/index.html') do
+	it {should exist}
+	its ('mode') { should cmp 0755 }
+	its ('owner') { should eq 'vagrant' }
+	its ('group') { should eq 'vagrant' }
+end
+
+describe file('/var/tmp/generator/populate_data.py') do
+	it {should exist}
+	its ('mode') { should cmp 0755 }
+	its ('owner') { should eq 'vagrant' }
+	its ('group') { should eq 'vagrant' }
+end
+
+#Test to verify crontab entry
+describe crontab do
+	its ('commands') { should include 'python /var/tmp/generator/populate_data.py'}
+	its('hours') { should cmp '5' }
+  	its('minutes') { should cmp '0' }
+end
+
